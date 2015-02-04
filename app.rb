03eb5f8697288ecc6beaed0c2c47696331122ee4
Debbie <cinemaestro@yahoo.com>
2015-02-04 15:56:27 -0800

    Add customer Sinatra pages

--- app.rb
+++ app.rb
@@ -7,3 +7,64 @@ get('/') do
   @customers = Customer.all()
   erb(:index)
 end
+
+post('/add_animal') do
+  animal_name = params.fetch("animal_name")
+  gender = params.fetch("gender")
+  date_of_admit = params.fetch("date_of_admit")
+  animal_type = params.fetch("animal_type")
+  breed = params.fetch("breed")
+  @animal_name = Animal.new({animal_name: animal_name, gender: gender, date_of_admit: date_of_admit, animal_type: animal_type, breed: breed})
+  if @animal_name.save()
+    redirect('/')
+  else
+    erb(:errors)
+  end
+end
+
+get('/animal/:id') do
+  @animal = Animal.find(params.fetch("id"))
+  erb(:animal)
+end
+
+patch('/animal/:id') do
+  animal_name = params.fetch("animal_name")
+  gender = params.fetch("gender")
+  date_of_admit = params.fetch("date_of_admit")
+  animal_type = params.fetch("animal_type")
+  breed = params.fetch("breed")
+  @animal = Animal.find(params.fetch("id").to_i)
+  @animal.update({animal_name: animal_name, gender: gender, date_of_admit: date_of_admit, animal_type: animal_type, breed: breed})
+  erb(:animal)
+end
+
+delete('animal/:id') do
+    animal = Animal.find(params.fetch("id").to_i)
+    animal.delete
+    redirect('/')
+end
+
+post '/add_customer' do
+  customer_name = params.fetch("customer_name")
+  type_preference = params.fetch("type_preference")
+  breed_preference = params.fetch("breed_preference")
+  @customer_name = Customer.new(customer_name: customer_name, type_preference: type_preference, breed_preference: breed_preference)
+  if @customer_name.save()
+    redirect('/')
+  else
+    erb(:errors)
+  end
+end
+
+get '/customer/:id' do
+  @customer = Customer.find(params.fetch("id"))
+  erb :customer
+end
+
+patch '/customer/:id' do
+  customer = Customer.find(params.fetch("id"))
+  params['check'].each do |check|
+    customer.bands << Animal.find(check.to_i)
+  end
+  redirect back
+end
